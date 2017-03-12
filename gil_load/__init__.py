8d1026621c4e5056d6fdaebaee5ff34c0c548597
Chris Billington <chrisjbillington@gmail.com>
2017-03-12 15:25:56 -0400

    Better starting and stopping with pthread conditions

--- gil_load/__init__.py
+++ gil_load/__init__.py
@@ -1,7 +1,7 @@
-from .gil_load import init, get
+from .gil_load import init, test, start, stop, get
 try:
     from .__version__ import __version__
 except ImportError:
     __version__ = None
 
-__all__ = ["init", "get"]
+__all__ = ["init", "test", "start", "stop", "get"]
--- gil_load/gil_load.pyx
+++ gil_load/gil_load.pyx
@@ -2,32 +2,76 @@ import sys
 import os
 import threading
 import ctypes
+cimport cython
+from libc.errno cimport ETIMEDOUT
 from libc.stdlib cimport malloc, free
 from libc.stdio cimport printf, fprintf, FILE, fdopen, fflush
 from libc.string cimport memcpy
 from libc.math cimport log
 from libc.time cimport time, time_t, localtime, strftime, tm
-from posix.time cimport timespec, nanosleep
+from posix.time cimport timespec, clockid_t, clock_gettime, CLOCK_MONOTONIC
+
+cdef extern from "pthread.h" nogil:
+
+    ctypedef struct pthread_cond_t:
+        pass
+    ctypedef struct pthread_mutex_t:
+        pass
+    ctypedef struct pthread_condattr_t:
+        pass
+    ctypedef struct pthread_mutexattr_t:
+        pass
+
+    int pthread_cond_init(pthread_cond_t *, pthread_condattr_t *)
+    int pthread_cond_signal(pthread_cond_t *)
+    int pthread_cond_timedwait(pthread_cond_t *, pthread_mutex_t *, timespec *)
+
+    int pthread_condattr_init(pthread_condattr_t *)
+    int pthread_condattr_setclock(pthread_condattr_t *, clockid_t)
+
+    int pthread_mutex_init(pthread_mutex_t *, pthread_mutexattr_t *)
+    int pthread_mutex_lock(pthread_mutex_t *)
+    int pthread_mutex_unlock(pthread_mutex_t *)
+
+
 cdef extern from "stdlib.h":
     double drand48() nogil
     int srand48(int) nogil
 
 
-# The pointer to the GIL (it's just an  int)
-cdef int * gil_ptr = NULL
-
+# The pointer to the GIL. It's a static int called gil_locked in
+# ceval_gil.h that is either 1 or 0 depending on whether the GIL is held.
+cdef int * gil_locked = NULL
 
 # The fraction of the time the GIL has been held:
 cdef double gil_load = 0
-
 
 # 1m, 5m, 15m averages:
 cdef double gil_load_1m = 0
 cdef double gil_load_5m = 0
 cdef double gil_load_15m = 0
 
+# The thread that is monitoring the GIL
+monitoring_thread = None
 
-LITTLE_ENDIAN = sys.byteorder == 'little'
+# A lock to make the functions in this module threadsafe
+lock = threading.Lock()
+
+
+cdef int LITTLE_ENDIAN = sys.byteorder == 'little'
+
+
+# A flag to tell the monitoring thread to stop, and an associated condition
+# and mutex to ensure we can wake it when sleeping and tell it to quit in a
+# race-free way.
+cdef int stopping = 0
+cdef pthread_cond_t cond
+cdef pthread_mutex_t mutex
+
+
+cdef int gil_held() nogil:
+    """Return whether the GIL is held by some thread"""
+    return gil_locked[0]
 
 
 cdef void mktimestamp(char* s) nogil:
@@ -39,22 +83,31 @@ cdef void mktimestamp(char* s) nogil:
     strftime(s, 26, "[%Y-%m-%d %H:%M:%S]", &tm_info)
 
 
-cdef void sleep(double seconds) nogil:
-    """Sleep for a given time in seconds. Returns immediately if seconds <= 0"""
-    if seconds <= 0:
-        return
-    cdef timespec remaining
-    remaining.tv_sec = <time_t>seconds
-    remaining.tv_nsec = <long>((seconds % 1) * 1000000000)
-    while True:
-        if nanosleep(&remaining, &remaining) == 0:
-            break
-        # otherwise assume EINTR
+cdef int sleep(double seconds) nogil:
+    """Sleep for a time in seconds or return immediately if seconds <= 0.
+   Other threads can interrupt the sleep by acquiring the mutex, setting
+   stopping = 1 and then calling pthread_cond_signal(cond). The caller of this
+   function must have acquired the mutex. Returns 1 if sleep was interrupted,
+   and zero otherwise."""
+    cdef timespec abstimeout
+    cdef int BILLION = 1000000000
+
+    clock_gettime(CLOCK_MONOTONIC, &abstimeout)
+    abstimeout.tv_sec += <time_t>seconds
+    abstimeout.tv_nsec += <long>((seconds % 1) * BILLION)
+    if abstimeout.tv_nsec > BILLION:
+        abstimeout.tv_sec += 1
+        abstimeout.tv_nsec -= BILLION
+
+    while not stopping:
+        if pthread_cond_timedwait(&cond, &mutex, &abstimeout) == ETIMEDOUT:
+            return 0
+    return 1
 
 
 def _get_data_segment():
-    """Get the data segment of process memory, in which the static variable
-    gil_locked from ceval_gil.h is located"""
+    """Get the data segment of process memory, in which gil_locked is
+    located"""
     with open('/proc/{}/maps'.format(os.getpid())) as f:
         # Dymanically linked?
         for line in f:
@@ -82,7 +135,7 @@ cdef int * get_gil_pointer() except NULL:
     cdef long i
 
     if threading.active_count() > 1:
-        raise RuntimeError("gil_load.start() must be called prior to other "
+        raise RuntimeError("gil_load.init() must be called prior to other "
                            "threads being started")
 
     ctypes.pythonapi.PyEval_InitThreads()
@@ -99,9 +152,10 @@ cdef int * get_gil_pointer() except NULL:
             else:
                 return <int*> &data_segment[i + 1 - sizeof(int)]
     else:
-        raise RuntimeError("Failed to find gil_locked variable")
+        raise RuntimeError("Failed to find gil in memory")
     
 
+@cython.cdivision(True)
 def _run(double av_sample_interval, double output_interval, output_file):
     """"""
     global gil_load
@@ -129,15 +183,10 @@ def _run(double av_sample_interval, double output_interval, output_file):
 
     srand48(time(NULL))
 
-    # # Test:
-    # printf("  gil: %d\n", gil_ptr[0])
-    # with nogil:
-    #     printf("nogil: %d\n", gil_ptr[0])
-
     with nogil:
-        while True:
-            sleep(-av_sample_interval * log(drand48()))
-            held = gil_ptr[0] != 0
+        pthread_mutex_lock(&mutex)
+        while not sleep(-av_sample_interval * log(drand48())):
+            held = gil_held()
             held_count += held
             check_count += 1
             gil_load = <double>held_count/<double>check_count
@@ -158,20 +207,109 @@ def _run(double av_sample_interval, double output_interval, output_file):
                 if output:
                     mktimestamp(timestamp)
                     fprintf(f, "%s  GIL load: %.2f (%.2f, %.2f, %.2f)\n",
-                            timestamp, gil_load, gil_load_1m, gil_load_5m, gil_load_15m)
+                            timestamp, gil_load,
+                            gil_load_1m, gil_load_5m, gil_load_15m)
                     fflush(f)
 
 
-def init(av_sample_interval=0.05, output_interval=5, output=None):
+def _checkinit():
+    if gil_locked == NULL:
+        raise RuntimeError("Must call gil_load.init() first")
+
+def init():
+    """Find the data structure for the GIL in memory so that we can monitor it
+    later to see how often it is held. This function must be called before any
+    other threads are started, and before calling start() to start monitoring
+    the GIL. Note: this function calls PyEval_InitThreads(), so if your
+    application was single-threaded, it will take a slight performance hit
+    from this, as the Python interpreter is not quite as efficient in
+    multithreaded mode as it is in single-threaded mode, even if there is only
+    one thread running."""
+    # Get a pointer to the GIL and store it as a global variable:
+    global gil_locked
+    with lock:
+        gil_locked = <int *> get_gil_pointer()
+
+    # Set up condition and mutex for telling the monitoring thread when to stop:
+    cdef pthread_condattr_t condattr
+    pthread_condattr_init(&condattr)
+    pthread_condattr_setclock(&condattr, CLOCK_MONOTONIC)
+    pthread_cond_init(&cond, &condattr)
+    pthread_mutex_init(&mutex, NULL)
+
+
+def start(av_sample_interval=0.05, output_interval=5, output=None, reset_counts=False):
+
+    """Start monitoring the GIL. Monitoring works by spawning a thread
+    (running only C code so as not to require the GIL itself), and checking
+    whether the GIL is held at random times. The random interval between times
+    is exponentially distributed with mean set by av_sample_interval. Over
+    time, statistics are accumulated for what proportion of the time the
+    GIL was held. Overall load, as well as 1 minute, 5 minute, and 15 minute
+    exponential moving averages are computed."""
+
+    _checkinit()
+
+    global gil_load
+    global gil_load_1m
+    global gil_load_5m
+    global gil_load_15m
+    global monitoring_thread
+
+    if reset_counts:
+        gil_load = gil_load_1m = gil_load_5m = gil_load_15m = 0
+
     if isinstance(output, str):
         output = open(output, 'a')
-    global gil_ptr
-    gil_ptr = get_gil_pointer()
-    thread = threading.Thread(target=_run,
-                              args=(av_sample_interval, output_interval, output))
-    thread.daemon = True
-    thread.start()
+
+    with lock:
+        if monitoring_thread is not None:
+            raise RuntimeError("GIL monitoring already started")
+        monitoring_thread = threading.Thread(target=_run,
+                                             args=(av_sample_interval, output_interval, output),
+                                             daemon=True)
+        monitoring_thread.start()
 
 
-def get():
-    return gil_load, (gil_load_1m, gil_load_5m, gil_load_15m)
+def stop():
+    """Stop monitoring the GIL. Accumulated statistics will still be available
+    with get()"""
+    global monitoring_thread
+    global stopping
+    with lock:
+        if monitoring_thread is None:
+            raise RuntimeError("GIL monitoring not running")
+        # Tell the monitoring thread to stop and then wait for it:
+        pthread_mutex_lock(&mutex)
+        stopping = 1
+        pthread_cond_signal(&cond)
+        pthread_mutex_unlock(&mutex)
+        monitoring_thread.join()
+        stopping = 0
+        monitoring_thread = None
+
+
+def get(N=2):
+    """Returns the average GIL load, and the 1m, 5m and 15m averages, rounded to N digits"""
+    _checkinit()
+    return round(gil_load, N), [round(n, N) for n in (gil_load_1m, gil_load_5m, gil_load_15m)]
+
+
+def test():
+    """Checks whether indeed the gil_held() function returns whether or not
+    the GIL is held."""
+
+    cdef int result
+
+    _checkinit()
+
+    if threading.active_count() > 1:
+        raise RuntimeError("Test only valid if no other threads running")
+    
+    assert gil_held() == 1, "gil_held() returned 0 when we were holding the GIL"
+
+    with nogil:
+         result = gil_held()
+    assert result == 0, "gil_held() returned 1 when we were not holding the GIL"
+    
+    return True
